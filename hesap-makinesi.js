d310e94048767d33214385375cd3055968a61557
Zehra Saraç <zehrassarac@gmail.com>
2024-03-27 09:38:58 +0300

    Create hesap-makinesi.js

---
+++ hesap-makinesi.js
@@ -0,0 +1,44 @@
+//hesap makinesi
+//değişkenler
+//operatörler
+//async metodu
+//if condition
+
+actor hesap_makinesi{
+    var hucre: Int = 0;
+    
+    //toplama
+    public func toplama(s : Int) : async Int {
+      hucre+=s;
+      hucre
+      // Debug.print(debug_show(hucre));)
+    };
+    //çıkarma
+    public func cikarma(s : Int) : async Int {
+      hucre -= s;
+      hucre
+    };
+    //çarpma
+    public func carpma(s : Int) : async Int {
+      hucre *= s;
+      hucre
+    };
+    //bölme
+    public func bolme(s : Int) : async ?Int {
+      if(s==0){
+        null 
+      }else{
+        hucre /= s;
+        ?hucre
+      };
+    };
+      //temizle
+      public func temizle() : async () {
+        hucre :=0
+      };
+    
+  
+  
+  
+  };
+  
